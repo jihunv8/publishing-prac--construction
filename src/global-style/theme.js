126023d0689c08541f5a2304f9fc19ee77ebd225
jihun <jihunv8@gmail.com>
2022-09-06 11:10:20 +0900

    [add] global-style 적용

---
+++ src/global-style/theme.js
@@ -0,0 +1,16 @@
+const color = {
+  black: '#313131',
+  gray: '#6B6B6B',
+  blue: '#40A2D5',
+};
+
+const contentsArea = {
+  large: '1170px',
+};
+
+const theme = {
+  color,
+  contentsArea,
+};
+
+export default theme;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import theme from './global-style/theme';
 
 import App from './App';
+import GlobalStyle from './global-style/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
